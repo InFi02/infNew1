1cbc2e002f613b7f362f483559d73a5c58a4539a
Payal Choudhary <xaverianpayalme@gmail.com>
2020-06-20 18:13:39 +0530

    ProfileHighlightsAdded

--- app/src/main/java/com/example/infi_project/data/model/HighlightsProfile.java
+++ app/src/main/java/com/example/infi_project/data/model/HighlightsProfile.java
@@ -2,64 +2,125 @@ package com.example.infi_project.data.model;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.infi_project.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HighlightsProfile#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+import Adapter.Profile_Highlight_Adapter;
+import Models.Post;
+
+
 public class HighlightsProfile extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+    Profile_Highlight_Adapter adapter;
+   List<Post> postList;
+
+    FirebaseUser fuser;
+    String mobileText;
+    String profileid;
+   
 
     public HighlightsProfile() {
-        // Required empty public constructor
+       
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HighlightsProfile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HighlightsProfile newInstance(String param1, String param2) {
-        HighlightsProfile fragment = new HighlightsProfile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    
+ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+       
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+       
         return inflater.inflate(R.layout.fragment_highlights_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fuser= FirebaseAuth.getInstance().getCurrentUser();
+        mobileText=fuser.getPhoneNumber();
+        profileid=mobileText.toString();
+
+        initRecyclerView();
+
+
+    }
+
+    private void initRecyclerView() {
+
+        recyclerView=(RecyclerView)getView().findViewById(R.id.recycler_view_highlight);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager=new GridLayoutManager(getContext(),3
+        );
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postList= new ArrayList<>();
+        adapter=new Profile_Highlight_Adapter(getContext(),postList);
+        recyclerView.setAdapter(adapter);
+        MyPhotos();
+
+    }
+
+    private void MyPhotos(){
+
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               postList.clear();
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                   Post post= snapshot.getValue(Post.class);
+                   // Toast.makeText(getContext(),post.getPublisher(),Toast.LENGTH_SHORT).show();
+                   System.out.println(post.getPublisher());
+                    if(post.getPublisher().equals(profileid)){
+                        postList.add(post);
+
+                    }
+                }
+
+                Collections.reverse(postList);
+                adapter.notifyDataSetChanged();
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
